e6d5470df8438ce7a2efdeaede0f8ff7ece22ba8
dorapon48 <dorapon48@icloud.com>
2024-01-19 15:08:33 +0900

    update: いろいろと追加

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 from huggingface_hub import snapshot_download
 model_id="elyza/ELYZA-japanese-Llama-2-7b-fast-instruct"
+# model_id="line-corporation/line-distilbert-base-japanese"
 snapshot_download(repo_id=model_id, local_dir="ELYZA-model",
     local_dir_use_symlinks=False, revision="main")
