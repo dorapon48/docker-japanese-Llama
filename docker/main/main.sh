04596a4d921d2ac3bd12ad8743ad4e96b2197d8a
dorapon48 <dorapon48@icloud.com>
2024-01-24 14:46:40 +0900

    update: n_gpu_layersを指定できるように変更, 起動するように変更

--- docker/main/main.sh
+++ docker/main/main.sh
@@ -3,4 +3,4 @@
 cd /app
 source ./.env
 
-python3 -m llama_cpp.server --model /data/models/${model_name}
+python3 -m llama_cpp.server --model /data/models/${model_name} --n_gpu_layers ${n_gpu_layers}
