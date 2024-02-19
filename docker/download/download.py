b956fb72f7d9f2a78b0352f92ca39590c3d489f2
dorapon48 <dorapon48@icloud.com>
2024-02-19 19:30:13 +0900

    update: binを優先的にダウンロードするように変更

--- docker/download/download.py
+++ docker/download/download.py
@@ -1,12 +1,42 @@
 import sys
-from huggingface_hub import snapshot_download
-# model_id = "elyza/ELYZA-japanese-Llama-2-7b-fast-instruct"
+from huggingface_hub import snapshot_download, HfApi
+
 model_id = sys.argv[1]
 file_name = sys.argv[2]
-# model_id = "elyza/ELYZA-japanese-Llama-2-7b-instruct"
-# file_name = "ELYZA-japanese-Llama-2-7b-instruct"
-if (model_id != "" and file_name != ""):
-    snapshot_download(repo_id=model_id, local_dir=file_name,
-        local_dir_use_symlinks=False, revision="main")
+
+api = HfApi()
+in_safetensor = False
+in_bin = False
+
+# リポジトリにあるファイルで，safetensorとbinが混在しているとき，
+# binを優先する
+file_list = list(api.list_repo_files(repo_id=model_id))
+
+for f_name in file_list:
+    if '.safetensors' in f_name:
+        in_safetensor = True
+        break
+for f_name in file_list:
+    if '.bin' in f_name:
+        in_bin = True
+        break
+
+# print(in_safetensor)
+
+if in_safetensor:
+    if in_bin:
+        if (model_id != "" and file_name != ""):
+            snapshot_download(repo_id=model_id, local_dir=file_name,
+                local_dir_use_symlinks=False, revision="main",
+                ignore_patterns=["*.safetensors","*.safetensors.index.json"])
+        else:
+            print("error: wrong argument")
+    else:
+        print("error: safetensor is not supported")
 else:
-    print("error:wrong argument")
+    if (model_id != "" and file_name != ""):
+        snapshot_download(repo_id=model_id, local_dir=file_name,
+            local_dir_use_symlinks=False, revision="main")
+    else:
+        print("error:wrong argument")
+
